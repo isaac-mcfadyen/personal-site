be8627c1ab6e5511304b2215bd7e8a5100f26b17
Isaac McFadyen <isaac@imcf.me>
2023-10-31 23:14:44 -0400

    chore: switch from markdoc to mdx, add Vectorize blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import markdoc from "@astrojs/markdoc";
 import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap(), prefetch(), markdoc(), svelte()],
+	integrations: [tailwind(), sitemap(), prefetch(), svelte(), mdx()],
 	site: "https://www.imcf.me",
 });
--- markdoc.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config";
-import prism from "@astrojs/markdoc/prism";
-
-export default defineMarkdocConfig({
-	extends: [prism()],
-});
