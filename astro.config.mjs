f56f47c0de6a7052d027c35fedf96fcf318ff98c
Isaac McFadyen <isaac@imcf.me>
2023-12-06 13:40:03 -0500

    feat: added new posts, upgraded Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap(), prefetch(), svelte(), mdx()],
+	integrations: [tailwind(), sitemap(), svelte(), expressiveCode(), mdx()],
 	site: "https://www.imcf.me",
 });
